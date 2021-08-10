331bd02f3c00402c08a8e6374859cb6284895a25
Anatolii <anatoliinikiforov11@gmail.com>
2021-08-11 02:22:23 +0800

    update blockchain

---
+++ src/print.rs
@@ -0,0 +1,11 @@
+pub fn run(){
+    // print to console
+    println!("Hello from the print.rs");
+
+    // Basic Formatting
+    println!("{} is from {}","Brad","Jhon");
+
+    // postional arguments
+
+
+}
